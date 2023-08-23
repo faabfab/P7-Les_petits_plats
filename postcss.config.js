ea49e434c4d55d93ea7680f8d06f4892f1a3f0b9
faabfab <faab.fab@free.fr>
2023-08-23 09:28:47 +0200

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/prefer-default-export
+export const plugins = {
+  'postcss-import': {},
+  tailwindcss: {},
+  autoprefixer: {},
+};
