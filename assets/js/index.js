0b50dda2fc78adf0c39459353a77649b6ab13878
faabfab <faab.fab@free.fr>
2023-09-02 10:33:53 +0200

    DOM events

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,5 +3,47 @@
 import { recipes } from '../data/recipes.js';
 
 import { getCards } from './utils/datas.js';
+import { searchEvent } from './utils/ModulesEvents.js';
 
-console.log(getCards(recipes))
+function init() {
+  // DOM init
+  getCards(recipes);
+
+  // DOM Events
+  // Input principal
+  // FIXME: reset après 3 caractères
+  const search = document.querySelector('#search');
+  const reset = document.querySelector('#search_reset');
+  reset.addEventListener('click', () => {
+    reset.setAttribute('class', 'hidden_div');
+  });
+  search.onkeyup = () => {
+    if (search.value.length >= 3) {
+      reset.removeAttribute('class');
+    }
+    else {
+      reset.setAttribute('class', 'hidden_div');
+    }
+  };
+}
+
+// filters
+// TODO: evenemets sur les filtres
+const filters = document.querySelectorAll('.filter')
+filters.forEach((filter) => {
+  const filterListState = filter.querySelector('.filter_list_state');
+  const filterListContent = filter.querySelector('.filter_list_content')
+  filter.addEventListener('click', () => {
+    if (filter.getAttribute('data-state') == 'close') {
+      filterListState.innerHTML = '<img src="/assets/img/list_close.svg" alt=""></img>';
+      filterListContent.classList.remove('hidden_div');
+      filter.setAttribute('data-state', 'open');
+    } else {
+      filterListState.innerHTML = '<img src="/assets/img/list_open.svg" alt=""></img>';
+      filterListContent.classList.add('hidden_div');
+      filter.setAttribute('data-state', 'close');
+    }
+  });
+});
+
+init();
---
+++ assets/js/utils/ModulesEvents.js
@@ -0,0 +1,6 @@
+function searchEvent() {
+  const search = document.querySelector('#search')
+  search.value = search.value+1;
+}
+
+export {searchEvent}
