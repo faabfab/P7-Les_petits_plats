e51f18de373230aa173172bf71d4b29b3be409c5
faabfab <faab.fab@free.fr>
2023-09-13 08:42:54 +0200

    filter tags

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,30 @@
 /* eslint-disable import/extensions */
-/* eslint-disable linebreak-style */
+// =============================================================================
+// IMPORTS
+// =============================================================================
 import { recipes } from '../data/recipes.js';
-import { recipesCounter } from './utils/ModulesEvents.js';
+import { recipesCounter, tagElement } from './utils/ModulesEvents.js';
 
-import { applianceArray, getCards, getFilterElements, ingredientsArray, ustensilsArray } from './utils/datas.js';
+import {
+  applianceArray,
+  getCards,
+  getFilterElements,
+  ingredientsArray,
+  ustensilsArray,
+} from './utils/datasDOMDisplay.js';
 
+/**
+ * Fonction d'initialisation du DOM
+ */
 function init() {
-  // DOM init
+  // init DOM
   getCards(recipes);
 
   getFilterElements(ingredientsArray(recipes), 'ingredients');
   getFilterElements(applianceArray(recipes), 'appliance');
   getFilterElements(ustensilsArray(recipes), 'ustensils');
 
-  // DOM Events
+  // Events on DOM
   // Input principal
   const search = document.querySelector('#search');
   const reset = document.querySelector('#search_reset');
@@ -29,7 +40,7 @@ function init() {
     }
   };
 
-  // filters
+  // Filters
   // TODO: Événements sur les filtres
   const filters = document.querySelectorAll('.filter')
   filters.forEach((filter) => {
@@ -37,12 +48,12 @@ function init() {
     const filterListState = filter.querySelector('.filter_list_state');
     const filterListContent = filter.querySelector('.filter_list_content')
     filterListBtn.addEventListener('click', () => {
-      if (filter.getAttribute('data-state') == 'close') {
-        filterListState.innerHTML = '<img src="/assets/img/list_close.svg" alt=""></img>';
+      if (filter.getAttribute('data-state') === 'close') {
+        filterListState.innerHTML = '<img src="/assets/img/list_close.svg" alt="list close"></img>';
         filterListContent.classList.remove('hidden_div');
         filter.setAttribute('data-state', 'open');
       } else {
-        filterListState.innerHTML = '<img src="/assets/img/list_open.svg" alt=""></img>';
+        filterListState.innerHTML = '<img src="/assets/img/list_open.svg" alt="list open"></img>';
         filterListContent.classList.add('hidden_div');
         filter.setAttribute('data-state', 'close');
       }
@@ -54,3 +65,22 @@ function init() {
 }
 
 init();
+
+// =============================================================================
+// Events after init
+// =============================================================================
+
+// filters
+const tagsArray = []; // tags selected array
+
+const filtersItems = document.querySelectorAll('.filter_list li');
+const itemsSelected = document.querySelector('#items_selected');
+filtersItems.forEach((filterItem) => {
+  filterItem.addEventListener('click', () => {
+    // FIXME: Afficher le tag correspondant
+    if (!tagsArray.includes(filterItem.textContent)) {
+      itemsSelected.appendChild(tagElement(filterItem.textContent, tagsArray));
+      //console.log(tagsArray);
+    }
+  });
+});
--- assets/js/utils/ModulesEvents.js
+++ assets/js/utils/ModulesEvents.js
@@ -4,4 +4,25 @@ function recipesCounter() {
   totalSpan.textContent = articles.length;
 }
 
-export { recipesCounter };
+function tagElement(name, tagsArray) {
+  const divItemSelected = document.createElement('div');
+  divItemSelected.setAttribute('class', 'item_selected');
+  const p = document.createElement('p');
+  p.textContent = name;
+  divItemSelected.append(p);
+  const divItemClose = document.createElement('div');
+  divItemClose.setAttribute('class', 'item_selected_close');
+  divItemClose.innerHTML = '<svg width="10" height="10" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 15L8.5 8.5M8.5 8.5L2 2M8.5 8.5L15 2M8.5 8.5L2 15" stroke="black" stroke-width="2.16667" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+  tagsArray.push(name);
+  divItemClose.addEventListener('click', () => {
+    divItemClose.parentElement.remove();
+    tagsArray.splice(tagsArray.indexOf(name), 1);
+  });
+  divItemSelected.appendChild(divItemClose);
+  return divItemSelected;
+}
+
+export {
+  recipesCounter,
+  tagElement,
+};
